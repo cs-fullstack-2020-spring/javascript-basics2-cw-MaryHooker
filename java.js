6a66483093be965625bd7c979b8a642d65aa9ae2
MaryHooker <mc020791@yahoo.com>
2020-02-14 13:53:35 -0600

    Word Problems

---
+++ java.js
@@ -0,0 +1,9 @@
+1.
+// let question=("My name is: ");
+// let name=("Mary");
+// console.log(question + name);
+
+2.
+// let balance=prompt("What is your balance?");
+// let subtract=(balance - 10);
+// console.log("You lost $10. Your new balance is " + "$" + subtract);

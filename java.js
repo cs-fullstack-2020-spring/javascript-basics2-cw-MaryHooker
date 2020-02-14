aef5b5c34b3b06bb4cb93e8b3eccd0508af8384e
MaryHooker <mc020791@yahoo.com>
2020-02-14 13:58:44 -0600

    Word Problems

--- java.js
+++ java.js
@@ -7,3 +7,10 @@
 // let balance=prompt("What is your balance?");
 // let subtract=(balance - 10);
 // console.log("You lost $10. Your new balance is " + "$" + subtract);
+
+3.
+// let grade=prompt("Enter your number grade.");
+// let credit=prompt("Enter your extra credit.");
+// console.log( parseInt(grade) + parseInt(credit));
+
+4.
